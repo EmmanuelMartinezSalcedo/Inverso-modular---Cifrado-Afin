2fb934fb5dec2186bc49c8d02a936fb4716701a2
EmmanuelMartinezSalcedo <90872703+EmmanuelMartinezSalcedo@users.noreply.github.com>
2022-05-08 18:49:42 -0500

    Update Inverso.py

--- Inverso.py
+++ Inverso.py
@@ -1,17 +1,24 @@
-def Euclides(n,m): #Usando algoritmo de euclides para saber si son coprimos
+def Euclides(n,m):
     if m==0:
         return n
     else:
         return Euclides(m,(n%m))  
-def inv(a,n):
-    e=Euclides(a,n)
-    if e!=1 or n<0:
-        print("No existe el inverso multiplicativo") #Si no son coprimos o n es negativo no existe inverso
-    elif e==1:
-        for x in range(1,n): #Por teoria el inverso se encuentra en un rango de 1 hasta n
-            if(((a%n)*(x%n) % n)==1): #Buscamos el menor x que cumpla la condicion:(a mod n)(x mod n) mod n = 1
-                return x #Retorna X
+
+def EuclidesEXT(n,m):
+    if m==0:
+        return (n,1,0)
+    else:
+        (d,x2,y2)=EuclidesEXT(m, n%m)
+        (x,y)=(y2,x2-(((n/m)-((n/m)%1))*y2))
+        return(d,x,y)
+
+def Inverso(a,n):
+    if Euclides(a,n)==1:
+        (d,x,y)=EuclidesEXT(a,n)
+        return (int(x%n))
+    else:
+        print("No existe el inverso multiplicativo")
 
 a=int(input())
 n=int(input())
-print(inv(a,n))
+print("Inverso modular: ",Inverso(a,n))
